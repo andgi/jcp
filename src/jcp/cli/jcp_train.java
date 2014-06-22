09dc80f13e4f3d3e84daf75098a6f71548c56e87
Anders Gidenstam <anders.gidenstam@hb.se>
2014-06-22 22:47:30 +0200

    jcp.cli.jcp_train: Added a test case that uses libsvm without conformal prediction.

--- src/jcp/cli/jcp_train.java
+++ src/jcp/cli/jcp_train.java
@@ -25,103 +25,229 @@ import jcp.io.*;
 
 public class jcp_train
 {
+    private DataSet _full;
+    private SortedSet<Double> _classSet;
+    private double[]          _classes;
+    private DataSet _training;
+    private DataSet _calibration;
+    private DataSet _test;
+
     public jcp_train()
     {
         super();
+        _training    = new DataSet();
+        _calibration = new DataSet();
+        _test        = new DataSet();
     }
 
     public void run(String[] args)
         throws IOException
     {
-        Random rand = new Random(new Date().getTime());
-
-        double n = 0;
-
-        DataSet full;
-        DataSet training    = new DataSet();
-        DataSet calibration = new DataSet();
-        DataSet test        = new DataSet();
-
         // Load and create training and calibration sets.
         if (args.length != 1) {
             System.out.println("Usage: jcp_train <libsvm formatted data set>");
             System.exit(-1);
         }
-
-        long t1 = System.currentTimeMillis();
         // FIXME: Only initial use case yet. Train & test.
-        FileInputStream file;
-        file = new FileInputStream(args[0]);
+        runICCTest(args);
+        //runPlainSVMTest(args);
+    }
 
-        full = new libsvmReader().read(file);
-        file.close();
+    private void runICCTest(String[] args)
+        throws IOException
+    {
+        long t1 = System.currentTimeMillis();
+        loadDataset(args[0]);
+        extractClasses();
         long t2 = System.currentTimeMillis();
-
-        System.out.println("Loaded the dataset " + args[0] + " containing " +
-                           full.x.rows() + " instances with " +
-                           full.x.columns() + " attributes.");
         System.out.println("Duration " + (double)(t2 - t1)/1000.0 + " sec.");
 
-        SortedSet<Double> classSet = new TreeSet<Double>();
-        for (int r = 0; r < full.x.rows(); r++) {
-            if (!classSet.contains(full.y[r])) {
-                classSet.add(full.y[r]);
-            }
-        }
-        double[] classes = new double[classSet.size()];
-        System.out.println("Classes: ");
-        int i = 0;
-        for (Double c : classSet.toArray(new Double[0])) {
-            classes[i] = c;
-            System.out.println("   " + classes[i]);
-            i++;
-        }
-
-        full.random3Partition(training, calibration, test, 0.4, 0.1);
+        splitDataset(0.4, 0.1);
         long t3 = System.currentTimeMillis();
-        System.out.println("Split the data set into training set, " +
-                           training.x.rows() + " instances, " +
-                           "calibration set, " +
-                           calibration.x.rows() + " instances, " +
-                           "and test set, " + test.x.rows() + " instances.");
         System.out.println("Duration " + (double)(t3 - t2)/1000.0 + " sec.");
 
-        System.out.println("Training on " + training.x.rows() +
+        System.out.println("Training on " + _training.x.rows() +
                            " instances and calibrating on " +
-                           calibration.x.rows() +
+                           _calibration.x.rows() +
                            " instances.");
 
 
         InductiveConformalClassifier icc =
-            new InductiveConformalClassifier(classes);
+            new InductiveConformalClassifier(_classes);
         icc._nc =
-            new SVMClassificationNonconformityFunction(classes);
+            new SVMClassificationNonconformityFunction(_classes);
 
-        icc.fit(training.x, training.y, calibration.x, calibration.y);
+        icc.fit(_training.x, _training.y, _calibration.x, _calibration.y);
         long t4 = System.currentTimeMillis();
         System.out.println("Training complete.");
         System.out.println("Duration " + (double)(t4 - t3)/1000.0 + " sec.");
 
-        System.out.println("Testing accuracy on " + test.x.rows() +
+        System.out.println("Testing accuracy on " + _test.x.rows() +
                            " instances.");
 
         // Evaluation on the test set.
         ObjectMatrix2D pred = null;
-        pred = icc.predict(test.x, 0.05);
+        pred = icc.predict(_test.x, 0.05);
         //System.out.println(pred);
 
-        for (i = 0; i < pred.rows(); i++){
-            int classIndex = classSet.headSet(test.y[i]).size();
+        int correct = 0;
+        int[] correctAtSize = new int[_classSet.size()+1];
+        int[] predictionAtSize = new int[_classSet.size()+1];
+
+        for (int i = 0; i < pred.rows(); i++){
+            int classIndex = _classSet.headSet(_test.y[i]).size();
+            int predictionSize = 0;
+            for (int c = 0; c < _classes.length; c++) {
+                if ((Boolean)pred.get(i, c)) {
+                    predictionSize++;
+                }
+            }
+            predictionAtSize[predictionSize]++;
+
             if ((Boolean)pred.get(i, classIndex)) {
-                n += 1;
+                correct++;
+                correctAtSize[predictionSize]++;
             }
         }
-
-        System.out.println("Accuracy " + (n / test.y.length));
         long t5 = System.currentTimeMillis();
+
+        System.out.println("Accuracy " + ((double)correct / _test.y.length));
+        for (int s = 0; s < predictionAtSize.length; s++) {
+            System.out.println("  Predictions with " + s + " classes: " +
+                               predictionAtSize[s]);
+            System.out.println
+                ("  Accuracy at prediction size of " + s + " classes: " +
+                 (double)correctAtSize[s]/(double)predictionAtSize[s]);
+        }
         System.out.println("Duration " + (double)(t5 - t4)/1000.0 + " sec.");
         System.out.println("Total Duration " + (double)(t5 - t1)/1000.0 +
                            " sec.");
+    }
+
+    private void runPlainSVMTest(String[] args)
+        throws IOException
+    {
+        long t1 = System.currentTimeMillis();
+        loadDataset(args[0]);
+        extractClasses();
+        long t2 = System.currentTimeMillis();
+        System.out.println("Duration " + (double)(t2 - t1)/1000.0 + " sec.");
+
+        splitDataset(0.4, 0.0);
+        long t3 = System.currentTimeMillis();
+        System.out.println("Duration " + (double)(t3 - t2)/1000.0 + " sec.");
+
+        System.out.println("Training on " + _training.x.rows() +
+                           " instances and calibrating on " +
+                           _calibration.x.rows() +
+                           " instances.");
+        // Default libsvm parameter.
+        jcp.bindings.libsvm.svm_parameter parameter =
+            new jcp.bindings.libsvm.svm_parameter();
+        parameter.svm_type = jcp.bindings.libsvm.svm_parameter.C_SVC;
+        parameter.kernel_type = jcp.bindings.libsvm.svm_parameter.RBF;
+        parameter.degree = 3;
+        parameter.gamma = 1.0/_classes.length;
+        parameter.coef0 = 0;
+        parameter.nu = 0.5;
+        parameter.cache_size = 100;
+        parameter.C = 1;
+        parameter.eps = 1e-3;
+        parameter.p = 0.1;
+        parameter.shrinking = 1;
+        parameter.probability = 1;
+        parameter.nr_weight = 0;
+        parameter.weight_label = new int[0];
+        parameter.weight = new double[0];
+
+        jcp.bindings.libsvm.svm_model model =
+            jcp.bindings.libsvm.svm.svm_train
+                (parameter,
+                 (jcp.bindings.libsvm.SparseDoubleMatrix2D)_training.x,
+                 _training.y);
+        long t4 = System.currentTimeMillis();
+        System.out.println("Training complete.");
+        System.out.println("Duration " + (double)(t4 - t3)/1000.0 + " sec.");
+
+        System.out.println("Testing accuracy on " + _test.x.rows() +
+                           " instances.");
+
+        // Evaluation on the test set.
+        int correct = 0;
+        double[] probability = new double[_classes.length];
+
+        for (int i = 0; i < _test.x.rows(); i++) {
+            int classIndex = _classSet.headSet(_test.y[i]).size();
+            double prediction =
+                jcp.bindings.libsvm.svm.svm_predict_probability
+                    (model,
+                     (jcp.bindings.libsvm.SparseDoubleMatrix1D)
+                         _test.x.viewRow(i),
+                     probability);
+            if (prediction == _test.y[i]) {
+                correct++;
+            }
+        }
+        long t5 = System.currentTimeMillis();
+
+        System.out.println("Accuracy " + ((double)correct / _test.y.length));
+        System.out.println("Duration " + (double)(t5 - t4)/1000.0 + " sec.");
+        System.out.println("Total Duration " + (double)(t5 - t1)/1000.0 +
+                           " sec.");
+    }
+
+    private void loadDataset(String filename)
+        throws IOException
+    {
+        FileInputStream file;
+        file = new FileInputStream(filename);
+
+        _full =
+            new libsvmReader().read
+                (file,
+                 // Select the desired representation.
+                 new cern.colt.matrix.impl.SparseDoubleMatrix2D(0, 0));
+                 //new jcp.bindings.libsvm.SparseDoubleMatrix2D(0, 0));
+        file.close();
+
+        System.out.println("Loaded the dataset " + filename + " containing " +
+                           _full.x.rows() + " instances with " +
+                           _full.x.columns() + " attributes.");
+    }
+
+    private void extractClasses()
+    {
+        _classSet = new TreeSet<Double>();
+        for (int r = 0; r < _full.x.rows(); r++) {
+            if (!_classSet.contains(_full.y[r])) {
+                _classSet.add(_full.y[r]);
+            }
+        }
+        _classes = new double[_classSet.size()];
+        System.out.println("Classes: ");
+        int i = 0;
+        for (Double c : _classSet.toArray(new Double[0])) {
+            _classes[i] = c;
+            System.out.println("   " + _classes[i]);
+            i++;
+        }
+    }
+
+    private void splitDataset(double trainingFraction,
+                              double calibrationFraction)
+    {
+        // Set template matrix types for the split data set.
+        _training.x = new jcp.bindings.libsvm.SparseDoubleMatrix2D(0, 0);
+        _calibration.x = _training.x;
+        _test.x = _training.x;
+        // Partition the full data set into training, calibartion and test sets.
+        _full.random3Partition(_training, _calibration, _test,
+                               trainingFraction, calibrationFraction);
+        System.out.println("Split the data set into training set, " +
+                           _training.x.rows() + " instances, " +
+                           "calibration set, " +
+                           _calibration.x.rows() + " instances, " +
+                           "and test set, " + _test.x.rows() + " instances.");
     }
 
     public static void main(String[] args)
