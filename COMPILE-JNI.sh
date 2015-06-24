0c73969e175a61a8a01394e3acbbbcc934688d51
Anders Gidenstam <anders.gidenstam@hb.se>
2015-06-24 16:58:34 +0200

    COMPILE-JNI.sh: Create the lib directory if it doesn't exist.

--- COMPILE-JNI.sh
+++ COMPILE-JNI.sh
@@ -1,2 +1,4 @@
+#/bin/bash
+mkdir -p lib
 # libsvm
 g++ -Wall -g -o lib/libsvm-jni.so jni/libsvm-jni.cpp -shared -fpic -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/ -I../libsvm.git/ -L./lib -lsvm
