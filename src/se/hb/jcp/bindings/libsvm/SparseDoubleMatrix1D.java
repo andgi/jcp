be42b4334e2daaf2f7b9931aa8062db55926447e
Anders Gidenstam <anders.gidenstam@hb.se>
2015-10-08 15:24:09 +0200

    Added the selected license: LGPL v3+.

--- src/se/hb/jcp/bindings/libsvm/SparseDoubleMatrix1D.java
+++ src/se/hb/jcp/bindings/libsvm/SparseDoubleMatrix1D.java
@@ -1,6 +1,20 @@
+// JCP - Java Conformal Prediction framework
 // Copyright (C) 2014  Anders Gidenstam
+//
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 // The public interface is based on cern.colt.matrix.DoubleMatrix1D.
-// License: to be defined.
 package se.hb.jcp.bindings.libsvm;
 
 import cern.colt.matrix.DoubleMatrix1D;
