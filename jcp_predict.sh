c391dfe9c8b0385225530d18ec5d9903564a6ce8
Anders Gidenstam <anders.gidenstam@hb.se>
2015-10-13 10:15:48 +0200

    Made the jcp_train.sh and jcp_predict.sh script wrappers set $LD_LIBRARY_PATH.

--- jcp_predict.sh
+++ jcp_predict.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# Expected environment:
+#  OCVJARDIR - directory with the OpenCV Java jar archives.
+#  OCVLIBDIR - directory with the OpenCV JNI library.
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# LD_LIBRARY_PATH - JCP JNI library and OCV JNI library (might not be needed).
+export LD_LIBRARY_PATH=$DIR/lib/:$OCVLIBDIR
 
 java -Xmx8192m -classpath ${DIR}/build/jar/jcp.jar:${DIR}/../colt/lib/colt.jar:${DIR}/../colt/lib/concurrent.jar:$OCVJARDIR/opencv.jar:${DIR}/../JSON/json.jar:${DIR}/../libsvm-java/libsvm.jar:${DIR}/../liblinear-java/liblinear-java.jar se.hb.jcp.cli.jcp_predict $@
