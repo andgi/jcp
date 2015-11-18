42eeec607d4880cb6a235a2edefe3496a3c5a359
Anders Gidenstam <anders.gidenstam@hb.se>
2015-11-18 18:27:19 +0100

    jcp.cp: Cleaned up the P-value computation and added the variant without smoothing.

--- src/se/hb/jcp/cp/Util.java
+++ src/se/hb/jcp/cp/Util.java
@@ -18,50 +18,26 @@
 package se.hb.jcp.cp;
 
 import java.util.Arrays;
-import java.util.Date;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Util
 {
-    private static Random _random = new Random(new Date().getTime());
-
-    @Deprecated
-    public static double[] calc_p_values(double[] nc_pred, double[] nc_cal)
-    {
-        double[] p_values = new double[nc_pred.length];
-        for (int i = 0; i < p_values.length; i++) {
-            int idx = Arrays.binarySearch(nc_cal, nc_pred[i]);           
-            if (idx < 0) {
-                p_values[i] = (nc_cal.length + (idx + 1) + 1) / (nc_cal.length + 1.0);
-            } else {
-                int count = 0;
-                for (int j = idx + 1; j < nc_cal.length && nc_pred[i] == nc_cal[j]; j++) { count++; }
-                for (int j = idx; j >= 0 && nc_pred[i] == nc_cal[j]; j--) { count++;  idx--;}
-                double p = (nc_cal.length - (idx + 1) - count) / (nc_cal.length + 1.0);
-                p_values[i] = p + ((count + 1) * (_random.nextDouble())) / (nc_cal.length + 1.0);
-            }
-        }
-        return p_values;
-    }
-    
-    @Deprecated
-    public static boolean[] calc_inclusion(double[] nc_pred, double[] nc_cal, double significance)
-    {
-        boolean[] include = new boolean[nc_pred.length];
-        double[] p_values = calc_p_values(nc_pred, nc_cal);
-        for (int i = 0; i < p_values.length; i++) {
-            include[i] = (p_values[i] >= significance);
-        }
-        return include;
-    }
+    private static final boolean USE_SMOOTHING = true;
 
     public static double calculatePValue(double nc_pred, double[] nc_cal)
     {
         double p_value;
         int idx = Arrays.binarySearch(nc_cal, nc_pred);
         if (idx < 0) {
+            // The key was not found, idx == -insertion_idx - 1 where
+            // insertion_idx is the the index of the first element
+            // greater than the search key.
             p_value = (nc_cal.length + (idx + 1) + 1) / (nc_cal.length + 1.0);
         } else {
+            // idx == one index containing the search key.
+            // Count the number of scores equal to nc_pred in nc_cal for
+            // smoothing according to [Vovk, ALRW WP#5, 2012] and place
+            // idx on the first score equal to nc_pred.
             int count = 0;
             for (int j = idx + 1;
                  j < nc_cal.length && nc_pred == nc_cal[j];
@@ -72,13 +48,18 @@ public class Util
                  j >= 0 && nc_pred == nc_cal[j];
                  j--) {
                 count++;
-                idx--;
+                idx = j;
             }
-            double p =
-                (nc_cal.length - (idx + 1) - count) / (nc_cal.length + 1.0);
-            p_value =
-                p + ((count + 1) * (_random.nextDouble())) /
-                (nc_cal.length + 1.0);
+            if (USE_SMOOTHING) {
+                // Smoothed p-value according to [Vovk, ALRW WP#5, 2012].
+                double theta = ThreadLocalRandom.current().nextDouble(1.0);
+                p_value =
+                    ((nc_cal.length - idx - count) + theta * (count + 1))
+                    / (nc_cal.length + 1.0);
+            } else {
+                // Unsmoothed p-value.
+                p_value = (nc_cal.length - idx + 1) / (nc_cal.length + 1.0);
+            }
         }
         return p_value;
     }
