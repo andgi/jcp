67c7dbdea72e8d1b2daabee9df32d7115d71e8fb
Anders Gidenstam <anders.gidenstam@hb.se>
2016-10-03 09:49:20 +0200

    Changed to use the java.library.path property instead of $LD_LIBRARY_PATH.

--- jcp_train.sh
+++ jcp_train.sh
@@ -6,7 +6,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# LD_LIBRARY_PATH - JCP JNI library and OCV JNI library (might not be needed).
-export LD_LIBRARY_PATH=$DIR/lib/:$OCVLIBDIR
-
-java -Xmx4096m -classpath ${DIR}/build/jar/jcp.jar:${DIR}/../colt/lib/colt.jar:${DIR}/../colt/lib/concurrent.jar:$OCVJARDIR/opencv.jar:${DIR}/../JSON/json.jar:${DIR}/../libsvm-java/libsvm.jar:${DIR}/../liblinear-java/liblinear-java.jar se.hb.jcp.cli.jcp_train $@
+java -Xmx4096m -classpath ${DIR}/build/jar/jcp.jar:${DIR}/../colt/lib/colt.jar:${DIR}/../colt/lib/concurrent.jar:$OCVJARDIR/opencv.jar:${DIR}/../JSON/json.jar:${DIR}/../libsvm-java/libsvm.jar:${DIR}/../liblinear-java/liblinear-java.jar -Djava.library.path=$DIR/lib/:$OCVLIBDIR se.hb.jcp.cli.jcp_train $@
