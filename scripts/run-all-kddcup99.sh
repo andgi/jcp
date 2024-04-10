5b4854f0f052ed8a1323808edbe722c5cb2bb1a2
Anders Gidenstam <anders.gidenstam@hb.se>
2024-04-10 13:53:16 +0200

    jcp: Updated the scripts for tests.
    Signed-off-by: Anders Gidenstam <anders.gidenstam@hb.se>

--- scripts/run-all-kddcup99.sh
+++ scripts/run-all-kddcup99.sh
@@ -4,16 +4,16 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 export TRAINING_SET=$DIR/../../pisvm-datasets/classification/kddcup99_nvr_train.scale.100000.dat
 
-export TEST_SET=$DIR/../../pisvm-datasets/classification/kddcup99_nvr_test.scale.dat
+export TEST_SET=$DIR/../../pisvm-datasets/classification/kddcup99_nvr_test.scale.5000.dat
 
 export MODELNAME=kddcup99_nvr
 
 export TRAINING_ARGS="-nc 0"
 
-$DIR/train-all.sh &> output.txt
+$DIR/train-all.sh &> output_$MODELNAME.txt
 
-echo >> output.txt
-echo >> output.txt
-echo >> output.txt
+echo >> output_$MODELNAME.txt
+echo >> output_$MODELNAME.txt
+echo >> output_$MODELNAME.txt
 
-$DIR/test-all.sh &>> output.txt
+$DIR/test-all.sh &>> output_$MODELNAME.txt
--- scripts/run-all-mnist576.sh
+++ scripts/run-all-mnist576.sh
@@ -2,18 +2,18 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-export TRAINING_SET=$DIR/../../pisvm-datasets/classification/mnist_train_576_rbf_8vr.500.dat
+export TRAINING_SET=$DIR/../../pisvm-datasets/classification/mnist_train_576_rbf_8vr.1000.dat
 
-export TEST_SET=$DIR/../../pisvm-datasets/classification/mnist_test_576_rbf_8vr.500.dat
+export TEST_SET=$DIR/../../pisvm-datasets/classification/mnist_test_576_rbf_8vr.5000.dat
 
 export MODELNAME=mnist_576_rbf_8vr
 
 export TRAINING_ARGS="-nc 0"
 
-$DIR/train-all.sh &> output.txt
+$DIR/train-all.sh &> output_$MODELNAME.txt
 
-echo >> output.txt
-echo >> output.txt
-echo >> output.txt
+echo >> output_$MODELNAME.txt
+echo >> output_$MODELNAME.txt
+echo >> output_$MODELNAME.txt
 
-$DIR/test-all.sh &>> output.txt
+$DIR/test-all.sh &>> output_$MODELNAME.txt
