5d90b0b8b7f784a45d7498e681c24b0c0551b35a
le-cam <tom.le-cam@ecole.ensicaen.fr>
2024-05-07 13:44:24 +0200

    Deleted OpenCv dependencies

--- src/se/hb/jcp/ml/ClassifierFactory.java
+++ src/se/hb/jcp/ml/ClassifierFactory.java
@@ -32,9 +32,7 @@ public final class ClassifierFactory
         {
             "se.hb.jcp.bindings.libsvm.SVMClassifier",
             "se.hb.jcp.bindings.jlibsvm.SVMClassifier",
-            "se.hb.jcp.bindings.jliblinear.LinearClassifier",
-            "se.hb.jcp.bindings.opencv.SVMClassifier",
-            "se.hb.jcp.bindings.opencv.RandomForestClassifier"
+            "se.hb.jcp.bindings.jliblinear.LinearClassifier"
         };
 
     private ClassifierFactory()
@@ -61,10 +59,6 @@ public final class ClassifierFactory
             return new se.hb.jcp.bindings.jlibsvm.SVMClassifier(config);
         case 2:
             return new se.hb.jcp.bindings.jliblinear.LinearClassifier(config);
-        case 3:
-            return new se.hb.jcp.bindings.opencv.SVMClassifier(config);
-        case 4:
-            return new se.hb.jcp.bindings.opencv.RandomForestClassifier(config);
         default:
             throw new UnsupportedOperationException("Unknown classifier type.");
         }
