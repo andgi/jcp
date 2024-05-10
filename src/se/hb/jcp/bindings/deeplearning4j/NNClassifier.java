83b1abb0b6a706084c6dada9ad4d6f210a97b0d4
le-cam <tom.le-cam@ecole.ensicaen.fr>
2024-05-10 11:45:04 +0200

    NN structure file

--- src/se/hb/jcp/bindings/deeplearning4j/NNClassifier.java
+++ src/se/hb/jcp/bindings/deeplearning4j/NNClassifier.java
@@ -1,17 +1,10 @@
 package se.hb.jcp.bindings.deeplearning4j;
 
-import org.deeplearning4j.nn.api.OptimizationAlgorithm;
-import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.layers.DenseLayer;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
-import org.nd4j.linalg.activations.Activation;
-import org.nd4j.linalg.lossfunctions.LossFunctions;
-import org.nd4j.linalg.dataset.DataSet; 
-import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.api.ndarray.INDArray;
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.nnet.learning.BackPropagation;
 
 import cern.colt.matrix.DoubleMatrix1D;
 import cern.colt.matrix.DoubleMatrix2D;
@@ -22,70 +15,67 @@ import se.hb.jcp.ml.ClassifierBase;
 import se.hb.jcp.ml.IClassProbabilityClassifier;
 import se.hb.jcp.ml.IClassifier;
 
-public class NNClassifier extends ClassifierBase implements IClassProbabilityClassifier{
-    
-    protected MultiLayerConfiguration _conf; 
-    protected MultiLayerNetwork _model; 
+public class NNClassifier extends ClassifierBase implements IClassProbabilityClassifier {
+    private static final SparseDoubleMatrix1D _storageTemplate =
+        new SparseDoubleMatrix1D(0);
+    protected NeuralNetwork _network;
 
     public NNClassifier() {
     }
+
     public NNClassifier(JSONObject configuration) {
         this();
-        //TOFIX I dont know if its work like this ? 
-        _conf = MultiLayerConfiguration.fromJson(configuration.toString());
+        //we can think about a json file with the number of features, then hidden layers and output 
     }
 
-    public NNClassifier(MultiLayerConfiguration configuration) {
-        _conf = configuration;
-        _model = new MultiLayerNetwork(_conf);
-        _model.init();
+    public NNClassifier(NeuralNetwork network) {
+        _network = network;
     }
 
     @Override
-    public IClassifier fitNew(DoubleMatrix2D x, double[] y) 
-    {
-        NNClassifier clone = new NNClassifier(_conf); 
-        clone.fit(x, y);
-        return clone;
+    public IClassifier fitNew(DoubleMatrix2D x, double[] y) {
+        NeuralNetwork newNetwork = createAndTrainNetwork(x, y);
+        return new NNClassifier(newNetwork);
     }
 
     @Override
-    public double predict(DoubleMatrix1D instance) 
-    {
-        INDArray input = Nd4j.create(instance.toArray());
-        INDArray output = _model.output(input);
-        double prediction = output.getDouble(0); 
-        return prediction;
+    public double predict(DoubleMatrix1D instance) {
+        return 0.0;
     }
-     public double predict(DoubleMatrix1D instance,
-                          double[] probabilityEstimates)
-    {
-        INDArray input = Nd4j.create(instance.toArray());
-        INDArray output = _model.output(input);
-        double prediction = output.getDouble(0); 
-        for (int i = 0; i < output.length(); i++) {
-            probabilityEstimates[i] = output.getDouble(i);
-        }
-        
-        return prediction;
 
+    public double predict(DoubleMatrix1D instance, double[] probabilityEstimates) {
+        return 0.0;
     }
 
     @Override
     public DoubleMatrix1D nativeStorageTemplate() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'nativeStorageTemplate'");
+        return _storageTemplate;
     }
 
     @Override
-    protected void internalFit(DoubleMatrix2D x, double[] y) 
-    {
-        INDArray features = Nd4j.create(x.toArray());
-        INDArray labels = Nd4j.create(y, new int[]{y.length, 1});
-        DataSet dataSet = new DataSet(features, labels);
-        MultiLayerNetwork model = new MultiLayerNetwork(_conf);
-        model.init();
-        model.fit(dataSet);
-        _model = model;
+    protected void internalFit(DoubleMatrix2D x, double[] y) {
+        // Implement training
+    }
+
+    private NeuralNetwork createAndTrainNetwork(DoubleMatrix2D x, double[] y) {
+       
+        NeuralNetwork neuralNetwork = new MultiLayerPerceptron(10, 20, 1);
+
+        DataSet dataSet = createDataSet(x, y);
+        
+        neuralNetwork.learn(dataSet);
+        return neuralNetwork;
+    }
+    private DataSet createDataSet(DoubleMatrix2D x, double[] y) {
+        DataSet dataSet = new DataSet(1000, y.length);
+
+        for (int i = 0; i < x.rows(); i++) {
+            double[] features = x.viewRow(i).toArray();
+            double label = y[i];
+            DataSetRow dataSetRow = new DataSetRow(features, new double[]{label});
+            //TOFIX
+            //dataSet.add(dataSetRow);
+        }
+        return dataSet;
     }
 }
