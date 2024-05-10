57fadd3e8eaa09e412621a97441d473f90859fe6
le-cam <tom.le-cam@ecole.ensicaen.fr>
2024-05-10 12:55:23 +0200

    Neural network - start of implementation of reading parameters

--- src/se/hb/jcp/bindings/deeplearning4j/NNClassifier.java
+++ src/se/hb/jcp/bindings/deeplearning4j/NNClassifier.java
@@ -5,6 +5,7 @@ import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.learning.BackPropagation;
+import org.neuroph.util.TransferFunctionType;
 
 import cern.colt.matrix.DoubleMatrix1D;
 import cern.colt.matrix.DoubleMatrix2D;
@@ -14,6 +15,8 @@ import org.json.JSONObject;
 import se.hb.jcp.ml.ClassifierBase;
 import se.hb.jcp.ml.IClassProbabilityClassifier;
 import se.hb.jcp.ml.IClassifier;
+
+import java.io.File;
 
 public class NNClassifier extends ClassifierBase implements IClassProbabilityClassifier {
     private static final SparseDoubleMatrix1D _storageTemplate =
@@ -26,8 +29,17 @@ public class NNClassifier extends ClassifierBase implements IClassProbabilityCla
     public NNClassifier(JSONObject configuration) {
         this();
         //we can think about a json file with the number of features, then hidden layers and output 
+        //and transfert fonction ? 
+        // and read weights ? 
+        _network = readJsonParammeters();
     }
 
+
+    public NNClassifier(String modelFilePath) {
+        this();
+        _network = NeuralNetwork.createFromFile(modelFilePath);
+    
+    }
     public NNClassifier(NeuralNetwork network) {
         _network = network;
     }
@@ -54,12 +66,12 @@ public class NNClassifier extends ClassifierBase implements IClassProbabilityCla
 
     @Override
     protected void internalFit(DoubleMatrix2D x, double[] y) {
-        // Implement training
+
     }
 
     private NeuralNetwork createAndTrainNetwork(DoubleMatrix2D x, double[] y) {
        
-        NeuralNetwork neuralNetwork = new MultiLayerPerceptron(10, 20, 1);
+        NeuralNetwork neuralNetwork = new MultiLayerPerceptron(TransferFunctionType.TANH, 2, 3, 1);
 
         DataSet dataSet = createDataSet(x, y);
         
@@ -77,5 +89,10 @@ public class NNClassifier extends ClassifierBase implements IClassProbabilityCla
             //dataSet.add(dataSetRow);
         }
         return dataSet;
+    }
+
+    private NeuralNetwork readJsonParammeters() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readJsonParammeters'");
     }
 }
